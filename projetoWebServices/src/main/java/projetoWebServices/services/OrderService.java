6b9595c9471513022a62d669c59eafbc8fc10f65
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-11 09:55:16 -0300

    feat(OrderService): implement service layer for Order with findAll and findById

--- projetoWebServices/src/main/java/projetoWebServices/services/OrderService.java
+++ projetoWebServices/src/main/java/projetoWebServices/services/OrderService.java
@@ -1,4 +1,27 @@
 package projetoWebServices.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import projetoWebServices.entities.Order;
+import projetoWebServices.repositories.OrderRepository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj =  repository.findById(id);
+        return obj.get();
+    }
+
 }
