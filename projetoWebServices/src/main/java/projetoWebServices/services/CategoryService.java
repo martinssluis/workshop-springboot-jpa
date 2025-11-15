5f49f819142793ede626af3ca1a59aa310c3a984
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-15 11:11:03 -0300

    feat(services): create CategoryService with methods to find all categories and find by id

--- projetoWebServices/src/main/java/projetoWebServices/services/CategoryService.java
+++ projetoWebServices/src/main/java/projetoWebServices/services/CategoryService.java
@@ -1,4 +1,27 @@
 package projetoWebServices.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import projetoWebServices.entities.Category;
+import projetoWebServices.repositories.CategoryRepository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj =  repository.findById(id);
+        return obj.get();
+    }
+
 }
