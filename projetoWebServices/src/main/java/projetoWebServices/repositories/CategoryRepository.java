2690ee99c3240f28e68bdc8d9f5d11c2a5f2f72a
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-15 11:09:31 -0300

    feat(repositories): create CategoryRepository interface extending JpaRepository for Category entity

--- projetoWebServices/src/main/java/projetoWebServices/repositories/CategoryRepository.java
+++ projetoWebServices/src/main/java/projetoWebServices/repositories/CategoryRepository.java
@@ -1,4 +1,7 @@
 package projetoWebServices.repositories;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import projetoWebServices.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
