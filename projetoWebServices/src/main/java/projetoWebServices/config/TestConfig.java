69079cbf2c89ce18ac3673dd422e567bcab24ee2
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-15 08:53:39 -0300

    feat(TestConfig): add Order implementation

--- projetoWebServices/src/main/java/projetoWebServices/config/TestConfig.java
+++ projetoWebServices/src/main/java/projetoWebServices/config/TestConfig.java
@@ -27,7 +27,7 @@ public class TestConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777779", "123456");
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); // associando com objeto User u1
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
