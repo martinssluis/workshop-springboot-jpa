68c54f6547651a4b4d9ea9c88e7a94969f195051
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-17 19:35:01 -0300

    feat(config): inject ProductRepository and instantiate sample Product entities in TestConfig

--- projetoWebServices/src/main/java/projetoWebServices/config/TestConfig.java
+++ projetoWebServices/src/main/java/projetoWebServices/config/TestConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import projetoWebServices.entities.Category;
 import projetoWebServices.entities.Order;
+import projetoWebServices.entities.Product;
 import projetoWebServices.entities.User;
 import projetoWebServices.repositories.CategoryRepository;
 import projetoWebServices.repositories.OrderRepository;
+import projetoWebServices.repositories.ProductRepository;
 import projetoWebServices.repositories.UserRepository;
 
 import java.time.Instant;
@@ -27,6 +29,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -42,8 +47,15 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p2, p2, p3, p4, p5));
     }
 }
