769c5fb0da15c8d9b85b285f02af23d47c1e26ea
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-22 20:57:32 -0300

    feat(config): add test data for OrderItem and save via OrderItemRepository in TestConfig

--- projetoWebServices/src/main/java/projetoWebServices/config/TestConfig.java
+++ projetoWebServices/src/main/java/projetoWebServices/config/TestConfig.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import projetoWebServices.entities.Category;
-import projetoWebServices.entities.Order;
-import projetoWebServices.entities.Product;
-import projetoWebServices.entities.User;
+import projetoWebServices.entities.*;
 import projetoWebServices.repositories.CategoryRepository;
 import projetoWebServices.repositories.OrderRepository;
+import projetoWebServices.repositories.OrderItemRepository;
 import projetoWebServices.repositories.ProductRepository;
 import projetoWebServices.repositories.UserRepository;
 
@@ -32,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -53,10 +54,16 @@ public class TestConfig implements CommandLineRunner {
         Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 
         p1.getCategories().add(cat2);
         p2.getCategories().add(cat1);
@@ -66,5 +73,6 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
