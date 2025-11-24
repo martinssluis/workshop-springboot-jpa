d172b08d1e60d1f9103f5e714646654df0fd7462
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-24 20:48:49 -0300

    feat(services): update method now throws ResourceNotFoundException when user is not found in UserService

--- projetoWebServices/src/main/java/projetoWebServices/services/UserService.java
+++ projetoWebServices/src/main/java/projetoWebServices/services/UserService.java
@@ -1,5 +1,6 @@
 package projetoWebServices.services;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
